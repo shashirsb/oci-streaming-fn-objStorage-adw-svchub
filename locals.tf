2ffbf1422963a2192de313822cca2ca4017b82b6
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2022-05-06 12:26:09 +0530

    fix the lb variables

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 locals {
-  bastion_public_ip                      = var.create_bastion_host == true ? module.bastion[0].bastion_public_ip : ""
+  /* bastion_public_ip                      = var.create_bastion_host == true ? module.bastion[0].bastion_public_ip : ""
   operator_private_ip                    = var.create_operator == true ? module.operator[0].operator_private_ip : ""
-  operator_instance_principal_group_name = var.create_operator == true ? module.operator[0].operator_instance_principal_group_name : ""
+  operator_instance_principal_group_name = var.create_operator == true ? module.operator[0].operator_instance_principal_group_name : "" */
   }
