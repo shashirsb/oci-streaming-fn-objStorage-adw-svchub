52eb2f3bd0e861e838038f5a2972f37168439008
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2022-05-07 22:15:51 +0530

    fix to variable tf

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -2,6 +2,8 @@ variable "bucket_name" {}
 variable "bucket_namespace" {}
 variable "bucket_object_events_enabled" {}
 variable "bucket_versioning" {}
+variable "compartment_id" {}
+variable "freeform_tags" {}
 
 
 
