fdfe4c57f77c682bbefc2538d90f7f4a32ec6f04
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2022-05-08 17:58:13 +0530

    fix

--- modules/db/adw.tf
+++ modules/db/adw.tf
@@ -6,11 +6,11 @@ resource "oci_database_autonomous_database" "adb_dw" {
   compartment_id           = var.compartment_id
   cpu_core_count           = var.cpu_core_count
   data_storage_size_in_tbs = var.data_storage_size_in_tbs
-  db_name                  = "${var.label_prefix}-${var.db_name}"
+  db_name                  = "${var.label_prefix}${var.db_name}"
   admin_password           = var.admin_password
   db_version               = var.db_version
   db_workload              = var.db_workload
-  display_name             = "${var.label_prefix}-${var.db_name}"
+  display_name             = "${var.label_prefix}${var.db_name}"
   is_free_tier             = var.is_free_tier
   license_model            = var.license_model
 }
