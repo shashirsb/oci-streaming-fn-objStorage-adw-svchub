d6955f782b41afb66a54bfe7e7210af165ad620d
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2022-05-09 21:09:20 +0530

    fix local oci function

--- modules/fn/locals.tf
+++ modules/fn/locals.tf
@@ -2,7 +2,7 @@
 
 locals {
   app_name_lower = lower(var.application_name)
-  ocir_repo_name = var.ocir_repo_name               // "cloudnative-2021/functions"  
+  ocir_repo_name = var.ocir_repo_name            
   ocir_docker_repository = join("", [lower("phx"), ".ocir.io"])
   ocir_namespace         = lookup(data.oci_objectstorage_namespace.os_namespace, "namespace")
   application_id = data.oci_functions_applications.function_applications.applications[0].id
