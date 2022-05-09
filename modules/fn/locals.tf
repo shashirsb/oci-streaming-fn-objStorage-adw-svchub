724730b38c93c0d0661868c5101433d1df6863ff
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2022-05-09 19:34:11 +0530

    fix

--- modules/fn/locals.tf
+++ modules/fn/locals.tf
@@ -3,7 +3,7 @@
 locals {
   app_name_lower = lower(var.application_name)
   ocir_repo_name = var.ocir_repo_name               // "cloudnative-2021/functions"  
-  ocir_docker_repository = join("", [lower(lookup(data.oci_identity_regions.oci_regions.regions[0], "key")), ".ocir.io"])
+  ocir_docker_repository = join("", [lower(var.region), ".ocir.io"])
   ocir_namespace         = lookup(data.oci_objectstorage_namespace.os_namespace, "namespace")
   application_id = data.oci_functions_applications.function_applications.applications[0].id
 
