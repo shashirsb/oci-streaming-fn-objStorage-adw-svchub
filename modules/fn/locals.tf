59af1792a9e72eafbe909918d07dfc7e0a793871
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2022-05-10 09:42:30 +0530

    fix the applicaiton id

--- modules/fn/locals.tf
+++ modules/fn/locals.tf
@@ -5,6 +5,6 @@ locals {
   ocir_repo_name = var.ocir_repo_name            
   ocir_docker_repository = join("", [lower("phx"), ".ocir.io"])
   ocir_namespace         = lookup(data.oci_objectstorage_namespace.os_namespace, "namespace")
-  application_id = "" //data.oci_functions_applications.function_applications.applications[0].id
+  application_id = data.oci_functions_applications.function_applications.applications[0].id
 
 }
