f071c3fe4521b6d6529441e44c8068594e0a6c29
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2022-05-11 12:46:35 +0530

    fix the appname

--- modules/fn/locals.tf
+++ modules/fn/locals.tf
@@ -1,7 +1,7 @@
 # OCIR repo name & namespace
 
 locals {
-  app_name_lower         = lower(var.application_names)
+  app_name_lower         = lower(var.application_names[0])
   ocir_repo_name         = var.ocir_repo_name            
   ocir_docker_repository = join("", [lower("phx"), ".ocir.io"])
   ocir_namespace         = lookup(data.oci_objectstorage_namespace.os_namespace, "namespace")
